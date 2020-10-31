f01e7f0aabf5458f7b638a4bab17ab66a1ffd3f1
Ayaneru658995 <1129726820@qq.com>
2020-10-31 14:55:24 +0800

    修改keep-alive

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <keep-alive include="home">
+    <keep-alive :include="cacheList">
       <router-view></router-view>
     </keep-alive>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style lang="less">
